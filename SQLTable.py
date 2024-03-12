ffe8344233d39a2b51bd4f7433db522d92699ef9
Nazar Yakubiv <nyakubiv@uwo.ca>
2024-03-12 11:30:33 -0400

    some message

---
+++ SQLTable.py
@@ -0,0 +1,13 @@
+#CREATING A TABLE AND DATABASE (DB) IN PYTHON FOR SQL
+import sqlite3
+connection = sqlite3.connect('testing.db')        #name/create a db file to use with sql 
+cursor = connection.cursor()        #lets us send and recieve commands back and forth
+
+cursor.execute( 
+    """ CREATE TABLE test (
+        iteration INTEGER,
+        multiplier REAL
+    )""")
+
+connection.commit() #saves changes 2121
+connection.close() #close the connection 
